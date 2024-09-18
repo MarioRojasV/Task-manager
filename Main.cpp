6192c057189f565d91fae20e20c6f910817cfc77
Fabián Vargas <fabidev18@gmail.com>
2024-09-18 14:10:38 -0600

    Primer parte terminada

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
+#include <string>
 
 using namespace std;
+
+struct Task;
+struct SubTask;
+struct Person;
+struct TaskType;
 
 template <class T>
 class List {
@@ -9,49 +18,86 @@ class List {
 
     List() {
       this->head = nullptr;
+      this->length = 0;
     }
 
-    void insertLast(T *newNode) {
-      if (this->head == nullptr) {
-        this->head = newNode;
-      } else {
-        T *currentNode = head;
-        this->recorrer(currentNode);
-        currentNode->next = newNode;
+    void insertLast(T* newNode) {
+      if (!head) head = newNode;
+      else {
+        T* lastNode = get(-1);
+        lastNode->next = newNode;
       }
-
-      ++ this->length;
+      ++length;
     }
 
-    void insertFirst(T *newNode) {
+    void insertFirst(T* newNode) {
       newNode->next = head;
       head = newNode;
-      ++ this->length;
+      ++length;
     }
 
-    T *get(const int index) {
-      T *currentNode = this->head;
-      this->recorrer(currentNode, index);
+    [[nodiscard]] T* get(int index) const {
+      if (index < 0) index = length + index;
+      T* currentNode = head;
+      recorrer(currentNode, index);
       return currentNode;
-    };
-
-    int getLength() {
-      return this->length;
     }
 
-  protected:
-    int length = 0;
-
-    static void recorrer(T *currentNode) {
-      while (currentNode != nullptr) {
+    [[nodiscard]] T* findById(int id) const {
+      T* currentNode = head;
+      while (currentNode) {
+        if (currentNode->id == id) return currentNode;
         currentNode = currentNode->next;
       }
+      return nullptr;
     }
-    static void recorrer(T *currentNode, const int maxCounter) {
-      int counter = 0;
-      while (currentNode != nullptr && counter <= maxCounter) {
+
+    T* removeById(int id) {
+      if (!head) return nullptr;
+
+      if (head->id == id) {
+        T* removedNode = head;
+        head = head->next;
+        --length;
+        return removedNode;
+      }
+
+      T* currentNode = head;
+      while (currentNode->next) {
+        if (currentNode->next->id == id) {
+          T* removedNode = currentNode->next;
+          currentNode->next = currentNode->next->next;
+          --length;
+          return removedNode;
+        }
         currentNode = currentNode->next;
-        counter ++;
+      }
+
+      return nullptr;
+    }
+
+    [[nodiscard]] string toString() const {
+      string rep = "{ ";
+      T* currentNode = head;
+      while (currentNode) {
+        rep += currentNode->name;
+        if (currentNode->next) rep += ", ";
+        currentNode = currentNode->next;
+      }
+      rep += " }";
+      return rep;
+    }
+
+    [[nodiscard]] int getLength() const { return this->length; }
+
+  protected:
+    int length{};
+
+    static void recorrer(T*& currentNode, int const maxCounter) {
+      int counter = 0;
+      while (currentNode && counter < maxCounter) {
+        currentNode = currentNode->next;
+        ++counter;
       }
     }
 };
@@ -98,23 +144,78 @@ struct Task {
   int id;
   string description;
   string importance;
-  string date;
-  string time;
+  tm date{};
+  tm time{};
   TaskType* type;
-  List<SubTask>* subTasks;
+  List<SubTask> subTasks;
   Task* next;
 
   Task(const int id, const string & description, const string & importance, const string & date, const string & time, TaskType * type) {
     this->id = id;
     this->description = description;
     this->importance = importance;
-    this->date = date;
-    this->time = time;
     this->type = type;
     this->next = nullptr;
+    this->date = {};
+    this->time = {};
 
     if (this->type->name == "Estudio") {
-      this->subTasks = new List<SubTask>();
+      this->subTasks = List<SubTask>();
+    }
+
+    setDate(date);
+    setTime(time);
+  }
+
+  void setDate(const string & date) {
+    istringstream dateStream(date);
+    dateStream >> get_time(&this->date, "%d-%m-%Y");
+    if (dateStream.fail()) throw runtime_error("Formato de fecha incorrecto. (dd-mm-YYYY)");
+  }
+
+  void setTime(const string & time) {
+    istringstream timeStream(time);
+    timeStream >> get_time(&this->time, "%H:%M:%S");
+    if (timeStream.fail()) throw runtime_error("Formato de hora incorrecto. (HH:MM:SS)");
+  }
+
+  [[nodiscard]] auto getDate() const {
+    return put_time(&this->date, "%d-%m-%Y");
+  }
+
+  [[nodiscard]] auto getTime() const  {
+    return put_time(&this->time, "%H:%M:%S");
+  }
+};
+
+class TaskList: public List<Task> {
+public:
+  TaskList() = default;
+
+  void insert(Task* task) {
+    if (!this->head) this->head = task;
+
+    else {
+      Task* currentNode = this->head;
+
+      while (currentNode != nullptr) {
+
+        if (task->date == currentNode->date) {
+          if (task->time == currentNode->time) {
+            task->next = currentNode->next;
+            currentNode->next = task;
+          } else if {
+
+          }
+
+        }
+
+        if (task->date > currentNode->date) {
+
+        }
+
+        currentNode = currentNode->next;
+      }
     }
   }
 };
@@ -142,9 +243,9 @@ struct Person {
   }
 };
 
-class CircularList : public List<TaskType> {
+class TaskTypeList : public List<TaskType> {
   public:
-    CircularList() {
+    TaskTypeList() {
       this->head = nullptr;
     }
 
@@ -159,7 +260,7 @@ class CircularList : public List<TaskType> {
 
       while (true) {
         if (currentNode->next == this->head) {
-          const auto newNode = new TaskType(currentNode->id + 1, name, description);
+          auto* const newNode = new TaskType(currentNode->id + 1, name, description);
           newNode->next = this->head;
           currentNode->next = newNode;
           this->length++;
@@ -170,9 +271,9 @@ class CircularList : public List<TaskType> {
     }
 };
 
-class DoubleList: public List<Person> {
+class PersonList: public List<Person> {
   public:
-    DoubleList() {
+    PersonList() {
       this->head = nullptr;
     }
 
@@ -226,6 +327,7 @@ class DoubleList: public List<Person> {
         if (currentNode->id == id) {
           currentNode->prev->next = currentNode->next;
           currentNode->next->prev = currentNode->prev;
+          --this->length;
           return currentNode;
         }
 
@@ -235,7 +337,7 @@ class DoubleList: public List<Person> {
       return nullptr;
     }
 
-    Person* get(const int index) {
+    [[nodiscard]] Person* get(const int index) const {
       if (index >= this->getLength()) return nullptr;
 
       Person* currentNode = this->head;
@@ -246,7 +348,7 @@ class DoubleList: public List<Person> {
       return currentNode;
     }
 
-    Person* getById(const int id) {
+    [[nodiscard]] Person* getById(const int id) const {
       Person* currentNode = this->head;
 
       while(currentNode != nullptr) {
@@ -259,27 +361,93 @@ class DoubleList: public List<Person> {
     }
 };
 
-
-
 // Tests
 
-auto people = DoubleList();
-auto taskTypes = CircularList();
+auto people = PersonList();
+auto taskTypes = TaskTypeList();
+
+void addTask(const int personId, Task* task) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    person->activeTasks.insertLast(task);
+}
+
+void addSubTask(const int personId, const int taskIndex, SubTask* subTask) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    Task* task = person->activeTasks.get(taskIndex);
+    if (!task) throw runtime_error("Tarea no encontrada");
+
+    if (task->type->name == "Estudio") {
+        task->subTasks.insertLast(subTask);
+    } else {
+        delete subTask;
+    }
+}
+
+void modifyActiveTask(const int personId, const int taskId, const string& newDate, const string& newTime) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    Task* task = person->activeTasks.get(taskId - 1);
+    if (!task) throw runtime_error("Tarea no encontrada");
+
+    task->setDate(newDate);
+    task->setTime(newTime);
+}
+
+void completeTask(const int personId, const int taskId) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    Task* task = person->activeTasks.removeById(taskId);
+    if (!task) throw runtime_error("Tarea no encontrada");
+
+    person->completedTasks.insertLast(task);
+}
+
+void completeSubTask(const int personId, const int taskId, const int subTaskIndex) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    Task* task = person->activeTasks.findById(taskId);
+    if (!task) throw runtime_error("Tarea no encontrada");
+
+    SubTask* subTask = task->subTasks.get(subTaskIndex);
+    if (!subTask) throw runtime_error("Subtarea no encontrada");
+
+    subTask->completed = true;
+    subTask->progress = 100;
+}
+
+void subTaskProgress(const int personId, const int taskId, const int subTaskIndex, const float newProgress) {
+    Person* person = people.findById(personId);
+    if (!person) throw runtime_error("Persona no encontrada");
+    Task* task = person->activeTasks.findById(taskId);
+    if (!task) throw runtime_error("Tarea no encontrada");
+
+    SubTask* subTask = task->subTasks.get(subTaskIndex);
+    if (!subTask) throw runtime_error("Subtarea no encontrada");
+
+    subTask->progress = newProgress;
+}
 
 void cargarDatos() {
   cout << "Cargando datos" << endl;
+
+  taskTypes.insert("Estudio", "Tareas y examenes");
+  taskTypes.insert("Hogar", "Tareas de la casa");
 
   people.insert(208620694, "Fabian", "Vargas", 19);
   people.insert(208620696, "Juan", "Perez", 20);
   people.insert(208620693, "Pedro", "Gonzalez", 21);
   people.insert(208620695, "Maria", "Lopez", 22);
 
-  taskTypes.insert("Universidad", "Tareas y examenes");
-  taskTypes.insert("Hogar", "Tareas de la casa");
+  addTask(208620694, new Task(1, "Examenes", "Medio", "01-09-2024", "12:00:00", taskTypes.get(0)));
+  addTask(208620694, new Task(2, "Proyecto Estructuras", "Alto", "20-09-2024", "08:00:00", taskTypes.get(0)));
+  addSubTask(208620694, 0, new SubTask("Calculo", "Estudiar ultimo tema", 65.3));
+  addSubTask(208620694, 1, new SubTask("Listas", "Listas de datos", 90));
 }
 
-int main() {
-  cargarDatos();
+void testing() {
+  cout << "Testeando funciones" << endl;
   cout << people.get(0)->name << endl;
   cout << people.get(1)->name << endl;
   cout << people.get(2)->name << endl;
@@ -291,7 +459,23 @@ int main() {
   cout << people.getById(208620695)->name << endl;
 
   cout << people.remove(208620695)->name << endl;
-  cout << people.getById(208620695) << endl; // Retora null
+  cout << people.getById(208620695) << endl; // Retora null/0
+
+  cout << people.getById(208620694)->activeTasks.get(0)->subTasks.get(0)->name << endl;
+  cout << people.getById(208620694)->activeTasks.get(0)->getTime() << endl;
+  modifyActiveTask(208620694, 1, "02-09-2024", "12:30:00");
+  cout << people.getById(208620694)->activeTasks.get(0)->getTime() << endl;
+
+  completeTask(208620694, 1);
+  cout << people.getById(208620694)->activeTasks.getLength() << endl; // Se espera 0 porque se completo la unica tarea que habia;
+  cout << people.getById(208620694)->completedTasks.get(0)->description << endl;
+
+  cout << people.getById(208620694)->completedTasks.get(0)->subTasks.toString();
+}
+
+int main() {
+  cargarDatos();
+  testing();
 
   return 0;
 }
